568ae444cae21801df98f980d240bdcb2c553c6f
glozanoa <glozanoa@uni.pe>
2021-12-13 09:16:53 -0500

    update lumache doc

--- lumache.py
+++ lumache.py
@@ -4,20 +4,34 @@ Lumache - Python library for cooks and food lovers.
 
 __version__ = "0.1.0"
 
+from time import sleep
 
 class InvalidKindError(Exception):
     """Raised if the kind is invalid."""
     pass
 
 
-def get_random_ingredients(kind=None):
+def get_random_ingredients(kind=None, sleeper: int = 1):
     """
     Return a list of random ingredients as strings.
 
     :param kind: Optional "kind" of ingredients.
     :type kind: list[str] or None
+    :param sleeper: Time to sleep
+    :type sleeper: int
     :raise lumache.InvalidKindError: If the kind is invalid.
     :return: The ingredients list.
     :rtype: list[str]
     """
     return ["shells", "gorgonzola", "parsley"]
+
+def sleeper(seconds: int):
+    """
+    Slepp by the supplied seconds
+
+    Arguments
+    =========
+    seconds: int
+        Seconds to sleep
+    """
+    sleep(seconds)  
